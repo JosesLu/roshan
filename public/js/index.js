dcc6af611a9119c10293fd57b921ecb148066e71
Joses Lu <Joses Lu>
2017-01-17 22:36:05 +0800

    ?

--- public/js/index.js
+++ public/js/index.js
@@ -120,7 +120,7 @@ function populateOne() { //One-way flights
 	}
 
 	$.ajax({
-		url: 'flights.JSON',
+		url: 'flights.json',
 		type: 'GET',
 		contentType: 'application/json; charset=utf-8',
 		dataType: 'JSON',
